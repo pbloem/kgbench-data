f12c5e36d228d2fc757d038bf077f90bb0a910ec
Peter Bloem <git@peterbloem.nl>
2020-12-16 23:28:30 +0100

    Add AIFB

--- datasets/aifb/raw/to_ints.py
+++ datasets/aifb/raw/to_ints.py
@@ -1,6 +1,8 @@
-import hdt, gzip, sys
+import hdt, gzip, sys, csv
 from tqdm import tqdm
 import pandas as pd
+
+import kgbench as kg
 
 
 """
@@ -8,33 +10,49 @@ import pandas as pd
 """
 
 doc = hdt.HDTDocument('aifb_stripped.hdt')
+
+entities = set()
+relations = set()
+datatypes = set()
+
 triples, c = doc.search_triples('', '', '')
-
-i2e = set()
-i2r = set()
-
 for s, p, o in tqdm(triples, total=c):
-    i2e.add(s)
-    i2r.add(p)
-    i2e.add(o)
+    datatypes.add(kg.entity_hdt(s)[1])
+    datatypes.add(kg.entity_hdt(o)[1])
 
-i2e = list(i2e)
-i2r = list(i2r)
+i2d = list(datatypes)
+i2d.sort()
+d2i = {d:i for i, d in enumerate(i2d)}
+
+triples, c = doc.search_triples('', '', '')
+for s, p, o in tqdm(triples, total=c):
+
+    se, sd = kg.entity_hdt(s)
+    oe, od = kg.entity_hdt(o)
+
+    entities.add((se, sd))
+    entities.add((oe, od))
+
+    relations.add(p)
+
+i2e = list(entities)
+i2r = list(relations)
 
 i2e.sort(); i2r.sort()
-# -- this is requires for the script to be deterministic
+# -- this is required for the script to be deterministic
+
+df = pd.DataFrame(enumerate(i2d), columns=['index', 'annotation'])
+df.to_csv('annotation-types.int.csv', index=False, header=True)
+
+ent_data = [(i, dt, ent) for i, (ent, dt) in enumerate(i2e)]
+df = pd.DataFrame(ent_data, columns=['index', 'annotation', 'label'])
+df.to_csv('nodes.int.csv', index=False, header=True, quoting=csv.QUOTE_NONNUMERIC)
+
+df = pd.DataFrame(enumerate(i2r), columns=['index', 'label'])
+df.to_csv('relations.int.csv', index=False, header=True, quoting=csv.QUOTE_NONNUMERIC)
 
 e2i = {e:i for i, e in enumerate(i2e)}
 r2i = {r:i for i, r in enumerate(i2r)}
-
-# print(i2e[5], e2i[i2e[5]])
-
-# Write entity and relation maps to CSV
-df = pd.DataFrame(enumerate(i2e), columns=['index', 'label'])
-df.to_csv('entities.int.csv', index=False, header=True)
-
-df = pd.DataFrame(enumerate(i2r), columns=['index', 'label'])
-df.to_csv('relations.int.csv', index=False, header=True)
 
 # Write triples to CSV
 print('Writing integer triples.')
@@ -42,7 +60,11 @@ triples, c = doc.search_triples('', '', '')
 with gzip.open('triples.int.csv.gz', 'wt') as file:
 
     for s, p, o in tqdm(triples, total=c):
-        file.write(f'{e2i[s]}, {r2i[p]}, {e2i[o]}\n')
+
+        sp = kg.entity_hdt(s)
+        op = kg.entity_hdt(o)
+
+        file.write(f'{e2i[sp]}, {r2i[p]}, {e2i[op]}\n')
 
 # Load test/train
 trainin = pd.read_csv('trainingSet.tsv', sep='\t')
@@ -53,7 +75,7 @@ print(all.columns)
 
 all.label_affiliation = pd.Categorical(all.label_affiliation)
 all['cls'] = all.label_affiliation.cat.codes
-all['instance'] = all.person.map(e2i)
+all['instance'] = all.person.map(lambda ent : e2i[(ent, 'iri')] )
 
 all = all[['instance', 'cls']]
 all.to_csv('all.int.csv', index=False, header=True)
