8a20563f85109f850297481c9f36eea41bd2825b
Peter Bloem <git@peterbloem.nl>
2022-12-09 15:52:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
-from setuptools import setup
+"""
+This package is deprecated please use the version at
 
-setup(
-    name="kgbench",
-    version="0.1",
-    description="A set of benchmark datasets for knowledge graph node classification",
-    url="https://kgbench.info",
-    author="Peter Bloem (Vrije Universiteit), Xander Wilcke (Vrije Universiteit), Lucas van Berkel, Victor de Boer (Vrije Universiteit)",
-    author_email="kgbench@peterbloem.nl",
-    packages=["kgbench"],
-    install_requires=[
-        "numpy",
-        "torch",
-        "torchvision",
-        "Pillow",
-        "scikit-image",
-        "rdflib",
-        "hdt"
-    ],
-    zip_safe=False
-)
+https://github.com/pbloem/kgbench-loader
+"""
+
+raise Exception('This package is deprecated please use the version at https://github.com/pbloem/kgbench-loader')
+
+# from setuptools import setup
+#
+# setup(
+#     name="kgbench",
+#     version="0.1",
+#     description="A set of benchmark datasets for knowledge graph node classification",
+#     url="https://kgbench.info",
+#     author="Peter Bloem (Vrije Universiteit), Xander Wilcke (Vrije Universiteit), Lucas van Berkel, Victor de Boer (Vrije Universiteit)",
+#     author_email="kgbench@peterbloem.nl",
+#     packages=["kgbench"],
+#     install_requires=[
+#         "numpy",
+#         "torch",
+#         "torchvision",
+#         "Pillow",
+#         "scikit-image",
+#         "rdflib",
+#         "hdt"
+#     ],
+#     zip_safe=False
+# )
